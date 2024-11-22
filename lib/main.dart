0e8f884ebc13c50b8bfbb088be753b222ba85253
kalutech <159139716+kalutech@users.noreply.github.com>
2024-11-22 11:42:10 +0200

    Some fixed 1

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() async{
   await WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Get.put(UserController());
-  Get.put(()=>RoomsController());
+  Get.put(RoomsController());
   runApp(const MyApp());
 }
 
